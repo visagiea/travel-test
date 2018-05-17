8594a7ed2752e9e59de19e0e90dac207152c142b
Anton <visagiea@gmail.com>
2018-05-17 15:11:23 +0200

    Completed automatic sprite generation

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,25 +4,24 @@ browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-	watch('./app/index.html', function() {
-		browserSync.reload();
-		//gulp.start('html');
-	});
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('cssinject');
-	});
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
 
 });
 
-gulp.task('cssinject', ['styles'], function() {
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
