c4b112ee57d6f94c605e66e44d64fd058d2dda0e
Anton <visagiea@gmail.com>
2018-07-03 18:25:36 +0200

    Completed desktop and mobile styles

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,23 @@
+import MobileMenu from './modules/MobileMenu';
+
+var mobileMenu = new MobileMenu();
+
+
+
+/*var $ = require('jquery');
+//Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " now owes $0 in taxes");
+	}
+}
+
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Adult("Jane Smith", "orange");
+jane.greet();
+jane.payTaxes();*/
